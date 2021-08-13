b3ea6ffa5207c16e4ea14057c04693283da4d919
Lyndsey Womack <lyndseywomack@lyndseys-air.lan>
2021-08-13 17:52:11 -0500

    angular exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photo Titles';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://thefreshplan.com/wp-content/uploads/2017/09/dachshund-puppies-weenie-dogs.jpg';
+  image3 = 'https://www.petage.com/wp-content/uploads/2019/09/Depositphotos_74974941_xl-2015-e1569443284386-670x627.jpg';
 
   constructor() { }
 
